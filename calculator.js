e4cd9bf1e0791fda6a89457d3e82aa8a511dd62f
Alliswell Godwin <47182522+Alliswell100@users.noreply.github.com>
2022-07-28 15:10:00 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,18 @@
+alert("connected")
+function addition(a, b){
+    var result = a+b;
+    return result;
+}
+function minus(a, b){
+    var result = a-b;
+    return result;
+   
+}
+function division(n, m){
+    var result = n/m;
+    return result;
+}
+function multiplication(m, n){
+    var result = m*n;
+    return result;
+}
